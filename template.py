aa971573a9538784eaa70bb7cbd793acee3f1c9e
Erik Liu <erikliu0801@gmail.com>
2019-11-27 16:28:04 +0800

    add template

---
+++ template.py
@@ -0,0 +1,36 @@
+# ToDo:
+
+"""
+Problem Description
+
+"""
+
+# Code
+## submit part
+
+## test part
+
+## code here
+
+
+# Test
+## Functional Test
+"""
+Condition:
+
+"""
+
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
