cf6e565efcffcbe1406c98147aba31a5e3606fa0
Erik Liu <erikliu0801@gmail.com>
2019-11-27 15:56:14 +0800

    brute force solution-> Success
    next step: hash map

--- P1_two_sum.py
+++ P1_two_sum.py
@@ -1,7 +1,8 @@
 # ToDo:
-# 1. Optimize to S1.2
-# 2. FT debug
-# 3. UT 
+#  3.Method:hash map
+#  Optimize to S2
+#  FT debug
+#  UT 
 
 """
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -32,7 +33,7 @@ def twoSum(nums, target):
 				return i, last_i
 
 
-## 1 Status:Wrong_Answer nums2 = [3,2,4], target = 6 -> 1.1
+## 1 brute force solution-> Status:Wrong_Answer nums2 = [3,2,4], target = 6 -> 1.1
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
     	for i, element in enumerate(nums):
@@ -42,18 +43,47 @@ class Solution:
     				return [i, last_i] #[0,1]
 
 
-## 1.1 Status:Time_Limit_Exceeded -> 1,2
+## 1.1 brute force solution-> Status:Time_Limit_Exceeded -> 1.2
 class Solution:
 	def twoSum(self, nums: List[int], target: int) -> List[int]:
 		for i, element in enumerate(nums):
 			for _last_i, last_element in enumerate(nums[i+1:]):
-				last_i = _last_i + i + 1
-				if element+last_element == target:
+				last_i = _last_i + i + 1		
+				if element+last_element == target:					
 					return [i, last_i] 
 
-## 1.2 
+## 1.2 brute force solution-> Success
+"""
+Runtime: 5232 ms, faster than 20.39% of Python3 online submissions for Two Sum.
+Memory Usage: 13.7 MB, less than 83.95% of Python3 online submissions for Two Sum.
+"""
 class Solution:
 	def twoSum(self, nums: List[int], target: int) -> List[int]:
+		for i, element in enumerate(nums):
+			for _last_i, last_element in enumerate(nums[i+1:]):			
+				if element+last_element == target:
+					last_i = _last_i + i + 1 #!!!
+					return [i, last_i] 
+
+
+
+## 2 y=target-x -> slower than 1.2
+"""
+Runtime: 5724 ms, faster than 9.30% of Python3 online submissions for Two Sum.
+Memory Usage: 13.7 MB, less than 79.07% of Python3 online submissions for Two Sum.
+"""
+class Solution:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
+		for i, element in enumerate(nums):
+			for _last_i, last_element in enumerate(nums[i+1:]):
+				if last_element == target - element:
+					last_i = _last_i + i + 1
+					return [i, last_i] 
+
+## 3 hash map
+class Solution:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
+
 
 # Test
 ## Functional Test
@@ -73,6 +103,10 @@ def twoSum_test(twoSum,maxLen:int,maxRange:int,times:int):
 		tlen =random.randint(1,maxLen)
 		for i in range(tlen):
 			nums_test.append(random.randint(1,max)) #Disorder
+		# make it hash
+		# make it be ordered
+
+
 		first_num = random.randint(0,tlen-1)
 		second_num = random.randint(0,tlen-1)
 		while first_num > second_num or first_num == second_num:
@@ -102,6 +136,9 @@ cProfile.run('twoSum([2, 7, 11, 15],9)')
 
 ## Unit Test
 import unittest
-class Test(unittest.TestCase):
+class Test_twoSum(unittest.TestCase):
 	def test(self):
 		pass
+
+if __name__ == '__main__':
+    unittest.main()
