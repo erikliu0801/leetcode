81cd32139d0e525d97c3ca3bc3d758b144904be5
Erik Liu <erikliu0801@gmail.com>
2019-12-07 17:21:10 +0800

    a bit of optimization

--- P3_ Longest_Substring_Without_Repeating_Characters.py
+++ P3_ Longest_Substring_Without_Repeating_Characters.py
@@ -111,8 +111,35 @@ Memory Usage: 17.8 MB, less than 5.10% of Python3 online submissions for Longest
 				max_count = x
 		return max_count
 
+#1.3
+"""
+Runtime: 328 ms, faster than 18.39% of Python3 online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 17.5 MB, less than 5.10% of Python3 online submissions for Longest Substring Without Repeating Characters.
+"""
+	def lengthOfLongestSubstring(self, s):
+		unrepeated_substring = []
+		substring = ""
+		for i, j in enumerate(list(s)): #
+			if substring.count(j) == 0:
+				substring = substring + j
+			else:
+				unrepeated_substring.append(substring)
+				for i, k in enumerate(list(substring)):
+					if k == j:
+						sub_substring = ""
+						for y in list(substring)[i + 1:]:
+							sub_substring = sub_substring + y
+						substring = sub_substring + j
+						break
+		unrepeated_substring.append(substring)
+		max_count = 0
+		for i, _ in enumerate(unrepeated_substring):
+			count = len(unrepeated_substring[i])
+			if count > max_count:
+				max_count = count
+		return max_count
 
-
+#2
 
 
 # Test
--- test.py
+++ test.py
@@ -1,25 +1,24 @@
 def lengthOfLongestSubstring(s):
     unrepeated_substring = []
-    string_list = list(s)
     substring = ""
-    for i, j in enumerate(string_list):
+    for i, j in enumerate(list(s)):  #
         if substring.count(j) == 0:
             substring = substring + j
         else:
             unrepeated_substring.append(substring)
             for i, k in enumerate(list(substring)):
                 if k == j:
-                    p_substring = ""
+                    sub_substring = ""
                     for y in list(substring)[i + 1:]:
-                        p_substring = p_substring + y
-                    substring = p_substring + j
+                        sub_substring = sub_substring + y
+                    substring = sub_substring + j
                     break
     unrepeated_substring.append(substring)
     max_count = 0
-    for i, j in enumerate(unrepeated_substring):
-        x = len(unrepeated_substring[i])
-        if x > max_count:
-            max_count = x
+    for i, _ in enumerate(unrepeated_substring):
+        count = len(unrepeated_substring[i])
+        if count > max_count:
+            max_count = count
     return max_count
 
 
@@ -27,4 +26,4 @@ if __name__ == "__main__":
     string01: str = "abcabcbb"
     string02 = " "
     string03 = "dvdf"
-    lengthOfLongestSubstring(string03)
+    print(lengthOfLongestSubstring(string03))
