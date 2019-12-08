f8042c72a6a03e4ed2f070c62f1d5b4670819570
Erik Liu <erikliu0801@gmail.com>
2019-12-09 00:37:58 +0800

    still stuck!!!!

--- P4_Median_of_Two_Sorted_Arrays.py
+++ P4_Median_of_Two_Sorted_Arrays.py
@@ -25,7 +25,7 @@ The median is (2 + 3)/2 = 2.5
 """
 # Concepts
 # 1. check ether len(nums1) and len(nums2) even
-# 2. 
+# 2. len(list), list[0], list[-1]
 
 # Code
 ## submit part
@@ -34,10 +34,38 @@ class Solution:
 ## test part
 
 ## code here
-#1
+#1 assume len(nums1, nums2) isPrime
 	def findMedianSortedArrays(self, nums1, nums2):
+		mid1, mid2 = len(nums1)//2, len(nums2)//2
+		answer = 0
+		while answer == 0:
+			if nums1[mid1] > nums2[mid2]:
+				# nums1 to left
+				mid1 = mid1//2
+				# nums2 to right
+				mid2 = mid2*3//2
+			elif nums1[mid1] < nums2[mid2]:
+				# nums2 to left
+				mid2 = mid2 // 2
+				# nums1 to right
+				mid1 = mid1 * 3 // 2
+			else:
+				answer = nums1[mid1]
+		return answer
+
+#2 assume len(nums1, nums2) isEven
 
 
+#3 Combine #1 & #2
+def findMedianSortedArrays(self, nums1, nums2):
+	if nums1[mid1] > nums2[mid2]:
+
+	elif nums1[mid1] < nums2[mid2]:
+
+	else:
+		if len(nums1) % 2 == 1 and len(nums2) % 2 == 1:
+			return nums1[mid1]
+		else
 
 
 # Test
--- test.py
+++ test.py
@@ -22,8 +22,30 @@ def lengthOfLongestSubstring(s):
     return max_count
 
 
+def findMedianSortedArrays(nums1, nums2):
+    mid1, mid2 = len(nums1) // 2, len(nums2) // 2
+    answer = 0
+    while answer == 0:
+        if nums1[mid1] > nums2[mid2]:
+            # nums1 to left
+            mid1 = mid1 // 2
+            # nums2 to right
+            mid2 = mid2 * 3 // 2
+        elif nums1[mid1] < nums2[mid2]:
+            # nums2 to left
+            mid2 = mid2 // 2
+            # nums1 to right
+            mid1 = mid1 * 3 // 2
+
+        else:
+            answer = nums1[mid1]
+    return answer
+
 if __name__ == "__main__":
-    string01: str = "abcabcbb"
-    string02 = " "
-    string03 = "dvdf"
-    print(lengthOfLongestSubstring(string03))
+    # string01: str = "abcabcbb"
+    # string02 = " "
+    # string03 = "dvdf"
+    # print(lengthOfLongestSubstring(string03))
+    nums1 = [3]
+    nums2 = [1,2,5]
+    print(findMedianSortedArrays(nums1,nums2))
