97272bf61998de2d0e3e1497129536e989c5aa75
Erik Liu <erikliu0801@gmail.com>
2019-12-24 12:04:56 +0800

    P118 success

---
+++ P119. Pascal's Triangle II.py
@@ -0,0 +1,73 @@
+# ToDo:
+
+"""
+119. Pascal's Triangle II
+Easy
+
+Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+
+Note that the row index starts from 0.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 3
+Output: [1,3,3,1]
+
+Follow up:
+
+Could you optimize your algorithm to use only O(k) extra space?
+
+
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+## test part
+def getRow(rowIndex):
+	"""
+	rowIndex: int
+	rtype: List[int]
+	"""
+## code here
+#1
+def getRow(rowIndex):
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+	input1 = []
+	expected_output = []
+	for i in range(len(input1)):
+		if func(input1[i]) != expected_output[i]:
+			print("Wrong!!!")
+			print(func(input1[i]))
+		else:
+			print("Right")		
+	# print(func(input1[-1]))
+	
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
