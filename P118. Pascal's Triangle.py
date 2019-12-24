c4cf257559fd0d66b9769e682b8cd6be0bc74ea6
Erik Liu <erikliu0801@gmail.com>
2019-12-24 12:04:56 +0800

    P118

--- P118. Pascal's Triangle.py
+++ P118. Pascal's Triangle.py
@@ -5,8 +5,6 @@
 Easy
 
 Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
-
-
 In Pascal's triangle, each number is the sum of the two numbers directly above it.
 
 Example:
@@ -37,7 +35,45 @@ def generate(numRows):
 	"""
 ## code here
 #1
+"""
+Success
+Runtime: 24 ms, faster than 94.56% of Python3 online submissions for Pascal's Triangle.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Pascal's Triangle.
+"""
 def generate(numRows):
+	def levelOfPascal(num):
+		if num == 1:
+			return [1]
+		elif num ==2:
+			return [1,1]
+		else:
+			pre_level = levelOfPascal(num-1)
+			for i in range(len(pre_level)-1):
+				pre_level[i] = pre_level[i] + pre_level[i+1]
+			level = [1] + pre_level
+			return level
+	if numRows == 0:
+		return 
+	else:
+		Triangle = []
+		for i in range(1,numRows+1):
+			Triangle.append(levelOfPascal(i))
+		return Triangle
+
+#1.1
+def levelOfPascal(num):
+	if num == 1:
+		return [1]
+	elif num ==2:
+		return [1,1]
+	else:
+		pre_level = levelOfPascal(num-1)
+		for i in range(len(pre_level)-1):
+			pre_level[i] = pre_level[i] + pre_level[i+1]
+		level = [1] + pre_level
+		return level
+
+
 
 # Test
 ## Functional Test
