8f862b33c32891466759acffe9f7430fe7ae37f2
Erik Liu <erikliu0801@gmail.com>
2019-12-26 13:46:46 +0800

    P183 success

--- P183. Customers Who Never Order.sql
+++ P183. Customers Who Never Order.sql
@@ -8,7 +8,6 @@ SQL Schema
 Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
 
 Table: Customers.
-
 +----+-------+
 | Id | Name  |
 +----+-------+
@@ -19,7 +18,6 @@ Table: Customers.
 +----+-------+
 
 Table: Orders.
-
 +----+------------+
 | Id | CustomerId |
 +----+------------+
@@ -28,7 +26,6 @@ Table: Orders.
 +----+------------+
 
 Using the above tables as example, return the following:
-
 +-----------+
 | Customers |
 +-----------+
@@ -50,3 +47,54 @@ Using the above tables as example, return the following:
 -- #1
 /*
 */
+SELECT Name AS Customers
+FROM Customers
+WHERE buy_times = 0
+
+-- #1.1
+/*
+{"headers": ["CustomerId", "buy_times"], "values": [[1, 1], [3, 1]]}
+*/
+SELECT CustomerId, COUNT(CustomerId) AS buy_times
+FROM Orders
+GROUP BY CustomerId
+
+-- #1.2
+/*
+{"headers": ["Id", "Name", "buy_times"], "values": [[1, "Joe", 1], [2, "Henry", null], [3, "Sam", 1], [4, "Max", null]]}
+*/
+SELECT C.Id, C.Name, O.buy_times
+FROM Customers C
+LEFT JOIN (
+	SELECT CustomerId, COUNT(CustomerId) AS buy_times
+	FROM Orders
+	GROUP BY CustomerId) O
+ON C.Id = O.CustomerId
+
+-- #1.3
+/*
+{"headers": ["Id", "Name", "buy_times"], "values": [[2, "Henry", null], [4, "Max", null]]}
+*/
+SELECT C.Id, C.Name, O.buy_times
+FROM Customers C
+LEFT JOIN (
+	SELECT CustomerId, COUNT(CustomerId) AS buy_times
+	FROM Orders
+	GROUP BY CustomerId) O
+ON C.Id = O.CustomerId
+WHERE O.buy_times IS null
+
+-- #1.4
+/*
+Success
+Runtime: 685 ms, faster than 28.02% of MySQL online submissions for Customers Who Never Order.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Customers Who Never Order.
+*/
+SELECT C.Name AS Customers
+FROM Customers C
+LEFT JOIN (
+	SELECT CustomerId, COUNT(CustomerId) AS buy_times
+	FROM Orders
+	GROUP BY CustomerId) O
+ON C.Id = O.CustomerId
+WHERE O.buy_times IS null
