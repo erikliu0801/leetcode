5459293d40e6467ebe12b2b724ca631586468901
Erik Liu <erikliu0801@gmail.com>
2019-12-26 18:22:25 +0800

    add P198

--- P197. Rising Temperature.sql
+++ P197. Rising Temperature.sql
@@ -24,9 +24,6 @@ For example, return the following Ids for the above Weather table:
 |  2 |
 |  4 |
 +----+
-
-
-
 -- Conditions & Concepts
 
 
@@ -36,8 +33,43 @@ For example, return the following Ids for the above Weather table:
 -- # Write your MySQL query statement below
 
 -- ## test part
+/*
+create table Weather(Id int, RecordDate char(30), Temperature int);
+insert into Weather values(1, '2015-01-01', 10);
+insert into Weather values(2, '2015-01-02', 25);
+insert into Weather values(3, '2015-01-03', 20);
+insert into Weather values(4, '2015-01-04', 30);
 
+*/
 -- ## code here
 -- #1
 /*
 */
+SELECT (Id + 1) temp_Id, Temperature
+FROM Weather
+
+-- 1.1
+/*
+Input: {"headers": {"Weather": ["Id", "RecordDate", "Temperature"]}, "rows": {"Weather": [[1, "2000-12-16", 3], [2, "2000-12-15", -1]]}}
+Output: {"headers": ["Id"], "values": []}
+Expected: {"headers":["Id"],"values":[[1]]}
+
+Misunderstanding
+*/
+SELECT W.Id, (W.Temperature - W0.Temperature) AS D
+FROM Weather W
+LEFT JOIN (
+	SELECT (Id + 1) temp_Id, Temperature
+	FROM Weather) W0
+ON W.Id = W0.temp_Id
+
+
+SELECT W.Id
+FROM Weather W
+LEFT JOIN (
+	SELECT (Id + 1) temp_Id, Temperature
+	FROM Weather) W0
+ON W.Id = W0.temp_Id
+WHERE (W.Temperature - W0.Temperature) > 0
+
+-- 1.2
---
+++ P198. House Robber.py
@@ -0,0 +1,77 @@
+# ToDo:
+
+"""
+198. House Robber
+Easy
+
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+
+
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+## test part
+def rob(nums):
+	"""
+	nums: List[int]
+	rtype: int
+	"""
+## code here
+#1
+def rob(nums):
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+	input1 = []
+	expected_output = []
+	for i in range(len(input1)):
+		if func(input1[i]) != expected_output[i]:
+			print("Wrong!!!")
+			print(func(input1[i]))
+		else:
+			print("Right")		
+	# print(func(input1[-1]))
+	
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
