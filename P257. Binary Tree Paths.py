d42578843d9ed205ad1fd3a1e7ebb10b232f04fc
Erik <erikliu0801@gmail.com>
2019-12-30 13:25:31 +0800

    P242 success

---
+++ P257. Binary Tree Paths.py
@@ -0,0 +1,94 @@
+# ToDo:
+
+"""
+257. Binary Tree Paths
+Easy
+
+1182
+
+82
+
+Add to List
+
+Share
+Given a binary tree, return all root-to-leaf paths.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+Output: ["1->2->5", "1->3"]
+
+Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+Accepted
+266.6K
+Submissions
+551.3K
+Seen this question in a real interview before?
+
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        
+## test part
+def binaryTreePaths(root):
+	"""
+	root: TreeNode
+	rtype: List[str]
+	"""
+## code here
+#1
+def binaryTreePaths(root):
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+	input1 = []
+	expected_output = []
+	for i in range(len(input1)):
+		if func(input1[i]) != expected_output[i]:
+			print("Wrong!!!")
+			print(func(input1[i]))
+		else:
+			print("Right")		
+	# print(func(input1[-1]))
+	
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
