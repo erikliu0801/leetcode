d4d1bbd71f5de1d35fdf8aeeb75d4f09e5e91062
Erik <erikliu0801@gmail.com>
2019-12-31 20:55:36 +0800

    P387, P389 success

---
+++ P392. Is Subsequence.py
@@ -0,0 +1,79 @@
+# ToDo:
+
+"""
+392. Is Subsequence
+Easy
+Given a string s and a string t, check if s is subsequence of t.
+
+You may assume that there is only lower case English letters in both s and t. t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+s = "abc", t = "ahbgdc"
+
+Return true.
+
+Example 2:
+s = "axc", t = "ahbgdc"
+
+Return false.
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+Credits:
+Special thanks to @pbrother for adding this problem and creating all test cases.
+
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+## test part
+def isSubsequence(s, t):
+	"""
+	s: str
+	t: str
+	rtype: bool
+	"""
+## code here
+#1
+def isSubsequence(s, t):
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+	input1 = []
+	expected_output = []
+	for i in range(len(input1)):
+		if func(input1[i]) != expected_output[i]:
+			print("Wrong!!!")
+			print(func(input1[i]))
+		else:
+			print("Right")		
+	# print(func(input1[-1]))
+	
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
