a1372d3a4d222f130257c32fc7efdd677019f09f
Erik <erikliu0801@gmail.com>
2019-12-31 19:57:52 +0800

    P349 success

--- P350. Intersection of Two Arrays II.py
+++ P350. Intersection of Two Arrays II.py
@@ -1,22 +1,28 @@
 # ToDo:
 
 """
-349. Intersection of Two Arrays
+350. Intersection of Two Arrays II
 Easy
+
 Given two arrays, write a function to compute their intersection.
 
 Example 1:
 
 Input: nums1 = [1,2,2,1], nums2 = [2,2]
-Output: [2]
+Output: [2,2]
 Example 2:
 
 Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-Output: [9,4]
+Output: [4,9]
 Note:
 
-Each element in the result must be unique.
+Each element in the result should appear as many times as it shows in both arrays.
 The result can be in any order.
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 
 """
 # Conditions & Concepts
@@ -26,10 +32,10 @@ The result can be in any order.
 # Code
 ## submit part
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
 ## test part
-def intersection(nums1, nums2):
+def intersect(nums1, nums2):
 	"""
 	nums1: List[int]
 	nums2: List[int]
@@ -37,14 +43,7 @@ def intersection(nums1, nums2):
 	"""
 ## code here
 #1
-"""
-Success
-Runtime: 44 ms, faster than 85.72% of Python3 online submissions for Intersection of Two Arrays.
-Memory Usage: 13 MB, less than 100.00% of Python3 online submissions for Intersection of Two Arrays.
-"""
-def intersection(nums1, nums2):
-	return list(set(nums1)&set(nums2))
-
+def intersect(nums1, nums2):
 
 # Test
 ## Functional Test
