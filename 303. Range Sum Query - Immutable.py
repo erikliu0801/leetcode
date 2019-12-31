e3459aa3576243b7b43103c306af4d60827cffc4
Erik <erikliu0801@gmail.com>
2019-12-31 19:04:31 +0800

    P303 success

--- 303. Range Sum Query - Immutable.py
+++ 303. Range Sum Query - Immutable.py
@@ -22,12 +22,30 @@ There are many calls to sumRange function.
 """
 # Code
 ## submit part
+class NumArray:
 
+	def __init__(self, nums: List[int]):
+		
+
+	def sumRange(self, i: int, j: int) -> int:
+	 
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
 ## test part
 
 ## code here
 #1
-
+"""
+Success
+Runtime: 1380 ms, faster than 5.40% of Python3 online submissions for Range Sum Query - Immutable.
+Memory Usage: 16.2 MB, less than 100.00% of Python3 online submissions for Range Sum Query - Immutable.
+"""
+class NumArray:
+	def __init__(self, nums: List[int]):
+		self.num_array = nums
+	def sumRange(self, i: int, j: int) -> int:
+		return sum(self.num_array[i:j+1])
 
 # Test
 ## Functional Test
@@ -59,4 +77,4 @@ class Test(unittest.TestCase):
 		pass
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
---
+++ P326. Power of Three.py
@@ -0,0 +1,90 @@
+# ToDo:
+
+"""
+326. Power of Three
+Easy
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+Example 3:
+
+Input: 9
+Output: true
+Example 4:
+
+Input: 45
+Output: false
+Follow up:
+Could you do it without using any loop / recursion?
+
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        
+## test part
+def isPowerOfThree(n):
+	"""
+	n: int
+	rtype: bool:
+	"""
+## code here
+#1
+"""
+ajust from P231 isPowerOfTwo(n)
+def isPowerOfTwo(n):
+	if n%2 == 1 and n != 1:
+		return False
+	else:
+		i = 0
+		while 2**i <= n:
+			if 2**i == n:
+				return True
+			i += 1
+		return False
+"""
+def isPowerOfThree(n):
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+	input1 = []
+	expected_output = []
+	for i in range(len(input1)):
+		if func(input1[i]) != expected_output[i]:
+			print("Wrong!!!")
+			print(func(input1[i]))
+		else:
+			print("Right")		
+	# print(func(input1[-1]))
+	
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
