4d72f6ff9b0e33ea4d6c1c18c016b32398e2c550
Erik <erikliu0801@gmail.com>
2020-01-12 19:49:27 +0800

    P485 success

---
+++ P492. Construct the Rectangle.py
@@ -0,0 +1,73 @@
+# ToDo:
+
+"""
+492. Construct the Rectangle
+Easy
+For a web developer, it is very important to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+
+1. The area of the rectangular web page you designed must equal to the given target area.
+
+2. The width W should not be larger than the length L, which means L >= W.
+
+3. The difference between length L and width W should be as small as possible.
+You need to output the length L and the width W of the web page you designed in sequence.
+Example:
+Input: 4
+Output: [2, 2]
+Explanation: The target area is 4, and all the possible ways to construct it are [1,4], [2,2], [4,1]. 
+But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. So the length L is 2, and the width W is 2.
+Note:
+The given area won't exceed 10,000,000 and is a positive integer
+The web page's width and length you designed must be positive integers.
+
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        
+## test part
+def constructRectangle(area):
+	"""
+	area: int
+	rtype: List[int]
+	"""
+## code here
+#1
+def constructRectangle(area):
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+	input1 = []
+	expected_output = []
+	for i in range(len(input1)):
+		if func(input1[i]) != expected_output[i]:
+			print("Wrong!!!")
+			print(func(input1[i]))
+		else:
+			print("Right")		
+	# print(func(input1[-1]))
+	
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
