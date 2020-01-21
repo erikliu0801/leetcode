67cd74502165661d321fb161d0d7b7bd1c05074f
Erik Liu <erikliu0801@gmail.com>
2020-01-21 17:45:43 +0800

    P125 success

--- python3/solved/P125. Valid Palindrome.py
+++ python3/solved/P125. Valid Palindrome.py
@@ -20,7 +20,7 @@ Output: false
 """
 # Conditions & Concepts
 """
-
+alphanumeric = alph + num
 """
 # Code
 ## submit part
@@ -35,10 +35,32 @@ def isPalindrome(s):
 	"""
 ## code here
 #1
+"""
+Wrong Answer
+Input:"0P"
+Output:true
+Expected:false
+
+"9;8'4P?X:1Q8`dOfJuJXD6FF,8;`Y4! YBy'Wb:ll;;`;\"JI0c2uvD':!LAk:s\"!'0.!2B55.T1VI?00Du?1,l``RFsc?Y?9vD5 K'3'1b!N8hn:'l. R:9:o`m1r.M2mrJ?`Wjv1`G6i6G`1vjW`?Jrm2M.r1m`o:::R .l':nh8N!b1'3'K 5Dv9?Y?csFR``l,1?uD00?IV1T.55B2!.0'!\"s:kAL!:'Dvu2c0IJ\";`;;ll9bW'yBY !4Y`;8,FF6DXJuJfOd`8Q1:X?P4'8;9"
+Output: true
+Expected: false
+
+Success
+Runtime: 72 ms, faster than 9.41% of Python3 online submissions for Valid Palindrome.
+Memory Usage: 13.2 MB, less than 94.05% of Python3 online submissions for Valid Palindrome.
+"""
 def isPalindrome(s):
-	if s == '':
-		return True
-	else:
+	s = s.lower()
+	s_pure = str()
+	alph = [chr(x) for x in range(97,123)]
+	alph_num = alph + [str(x) for x in range(0,10)]
+	for c in s:
+		if c in alph_num:
+			s_pure = s_pure + c
+	return s_pure == s_pure[::-1]
+
+
+
 		
 
 # Test
