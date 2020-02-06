a99f66d58bc81662603ab427c755d533da571446
Erik Liu <erikliu0801@gmail.com>
2020-02-06 19:00:52 +0800

    P657 success

---
+++ python3/solved/P657. Robot Return to Origin.py
@@ -0,0 +1,82 @@
+# ToDo:
+
+"""
+657. Robot Return to Origin
+Easy
+
+There is a robot starting at position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+
+The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down). If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
+
+Note: The way that the robot is "facing" is irrelevant. "R" will always make the robot move to the right once, "L" will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.
+
+Example 1:
+
+Input: "UD"
+Output: true 
+Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
+
+ 
+
+Example 2:
+
+Input: "LL"
+Output: false
+Explanation: The robot moves left twice. It ends up two "moves" to the left of the origin. We return false because it is not at the origin at the end of its moves.
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        
+## test part
+def judgeCircle(moves):
+	"""
+	moves: str
+	rtype: bool
+	"""
+## code here
+#1
+"""
+Success
+Runtime: 32 ms, faster than 91.86% of Python3 online submissions for Robot Return to Origin.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Robot Return to Origin.
+"""
+def judgeCircle(moves):
+	return moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R')
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+	input1 = []
+	expected_output = []
+	for i in range(len(input1)):
+		if func(input1[i]) != expected_output[i]:
+			print("Wrong!!!")
+			print(func(input1[i]))
+		else:
+			print("Right")		
+	# print(func(input1[-1]))
+	
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
