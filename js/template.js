b2f7c94a071f4fac3b41e836865843157382aa85
Erik Liu <erikliu0801@gmail.com>
2020-02-13 13:12:21 +0800

    start solve problems in js

---
+++ js/template.js
@@ -0,0 +1,25 @@
+// ToDo:
+/* */
+
+// Problem Description
+/*
+
+*/
+
+// Conditions & Concepts
+/* */
+
+// Code
+// submit part
+
+// test part
+/* */
+
+// code here
+// 1
+/* */
+function () {
+
+} 
+
+// Test
