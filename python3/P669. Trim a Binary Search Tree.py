d83ed5505ef87d4d3c9c36ac7d3b19f84fb1cf29
Erik Liu <erikliu0801@gmail.com>
2020-02-19 18:00:22 +0800

    add P669

---
+++ python3/P669. Trim a Binary Search Tree.py
@@ -0,0 +1,108 @@
+# ToDo:
+
+"""
+669. Trim a Binary Search Tree
+Easy
+
+Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+
+Example 1:
+
+Input: 
+    1
+   / \
+  0   2
+
+  L = 1
+  R = 2
+
+Output: 
+    1
+      \
+       2
+
+Example 2:
+
+Input: 
+    3
+   / \
+  0   4
+   \
+    2
+   /
+  1
+
+  L = 1
+  R = 3
+
+Output: 
+      3
+     / 
+   2   
+  /
+ 1
+
+
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def trimBST(self, root: TreeNode, L: int, R: int) -> TreeNode:
+        
+## test part
+class Solution:
+    def trimBST(self, root, L, R):
+    	"""
+    	root: TreeNode
+    	L: int
+    	R: int
+    	rtype: TreeNode
+    	"""
+  
+## code here
+#1
+class Solution:
+    def trimBST(self, root, L, R):
+
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+	input1 = []
+	expected_output = []
+	for i in range(len(input1)):
+		if func(input1[i]) != expected_output[i]:
+			print("Wrong!!!", ' Output:', func(input1[i]), '; Expected Output:', expected_output[i])
+		else:
+			print("Right")
+	# print(func(input1[-1]))
+	
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+unittest.main()
