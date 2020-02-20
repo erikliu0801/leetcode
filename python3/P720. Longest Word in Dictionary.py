d456472540a7cbf23560eaa7ecb48bf2e1709c5a
Erik Liu <erikliu0801@gmail.com>
2020-02-20 16:15:45 +0800

    add P720

---
+++ python3/P720. Longest Word in Dictionary.py
@@ -0,0 +1,84 @@
+# ToDo:
+
+"""
+720. Longest Word in Dictionary
+Easy
+
+Given a list of strings words representing an English Dictionary, find the longest word in words that can be built one character at a time by other words in words. If there is more than one possible answer, return the longest word with the smallest lexicographical order.
+If there is no answer, return the empty string.
+
+Example 1:
+
+Input: 
+words = ["w","wo","wor","worl", "world"]
+Output: "world"
+Explanation: 
+The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+
+Example 2:
+
+Input: 
+words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+Output: "apple"
+Explanation: 
+Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is lexicographically smaller than "apply".
+
+Note:
+All the strings in the input will only contain lowercase letters.
+The length of words will be in the range [1, 1000].
+The length of words[i] will be in the range [1, 30].
+
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        
+## test part
+class Solution:
+    def longestWord(self, words):
+    	"""
+    	words: List[str]
+    	rtype: str
+    	"""
+
+## code here
+#1
+class Solution:
+    def longestWord(self, words):
+
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+	input1 = []
+	expected_output = []
+	for i in range(len(input1)):
+		if func(input1[i]) != expected_output[i]:
+			print("Wrong!!!", ' Output:', func(input1[i]), '; Expected Output:', expected_output[i])
+		else:
+			print("Right")
+	# print(func(input1[-1]))
+	
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+	def test(self):
+		pass
+
+if __name__ == '__main__':
+unittest.main()
