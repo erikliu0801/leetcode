d2e539e1cac23bf7fee176695a8b0e5a0a748e41
Erik Liu <erikliu0801@gmail.com>
2020-02-20 16:12:37 +0800

    P706 success

---
+++ python3/solved/P706. Design HashMap.py
@@ -0,0 +1,145 @@
+# ToDo:
+
+"""
+706. Design HashMap
+Easy
+
+Design a HashMap without using any built-in hash table libraries.
+
+To be specific, your design should include these functions:
+
+    put(key, value) : Insert a (key, value) pair into the HashMap. If the value already exists in the HashMap, update the value.
+    get(key): Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+    remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.
+
+
+Example:
+
+MyHashMap hashMap = new MyHashMap();
+hashMap.put(1, 1);          
+hashMap.put(2, 2);         
+hashMap.get(1);            // returns 1
+hashMap.get(3);            // returns -1 (not found)
+hashMap.put(2, 1);          // update the existing value
+hashMap.get(2);            // returns 1 
+hashMap.remove(2);          // remove the mapping for 2
+hashMap.get(2);            // returns -1 (not found) 
+
+
+Note:
+
+    All keys and values will be in the range of [0, 1000000].
+    The number of operations will be in the range of [1, 10000].
+    Please do not use the built-in HashMap library.
+
+
+
+"""
+# Conditions & Concepts
+"""
+
+"""
+# Code
+## submit part
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
+## test part
+
+## code here
+#1
+"""
+Success
+Runtime: 192 ms, faster than 91.50% of Python3 online submissions for Design HashMap.
+Memory Usage: 15.2 MB, less than 100.00% of Python3 online submissions for Design HashMap.
+"""
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hashmap = {}
+        
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        self.hashmap[key] = value
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        if key in self.hashmap:
+            return self.hashmap[key]
+        else: return -1
+        
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        if key in self.hashmap:
+            self.hashmap.pop(key)
+
+
+# Test
+## Functional Test
+"""
+# Conditions & Concepts
+
+"""
+if __name__ == '__main__':
+    input1 = []
+    expected_output = []
+    for i in range(len(input1)):
+        if func(input1[i]) != expected_output[i]:
+            print("Wrong!!!", ' Output:', func(input1[i]), '; Expected Output:', expected_output[i])
+        else:
+            print("Right")
+    # print(func(input1[-1]))
+    
+
+## Performance Test
+import cProfile
+cProfile.run('')
+
+
+## Unit Test
+import unittest
+class Test(unittest.TestCase):
+    def test(self):
+        pass
+
+if __name__ == '__main__':
+unittest.main()
